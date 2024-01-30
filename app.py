3b5d6b7abdb28a9b22eb1c1a96e06e8931af6d13
Arvind Sivakumar <96773379+Programmer-Arvind@users.noreply.github.com>
2024-01-30 11:39:27 +0530

    Displayed the output and errors handled

--- app.py
+++ app.py
@@ -4,39 +4,33 @@ from web3 import Web3, exceptions
 
 app = Flask(__name__)
 
-infura_url = "Infura_URL"
-web3 = Web3(Web3.HTTPProvider(infura_url))
 
 @app.route('/', methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        print(request.form["Endpoint"], request.form["Address"])
-        return render_template('index.html')
+        infura_url, address = request.form["Endpoint"], request.form["Address"]
+        web3 = Web3(Web3.HTTPProvider(infura_url))
+        connect_status = web3.is_connected()
+        if connect_status:
+            print("Successfully connected!")
+            balance = get_balance(address, web3)
+            if not balance:
+                return render_template('index.html', invalid_address=True)
+            return render_template('index.html', balance=balance)
+        return render_template('index.html', invalid_endpoint=True)
     return render_template('index.html')
 
 
 
 @app.route('/balance/<address>')
-def get_balance(address):
+def get_balance(address, web3):
     print(f"{web3.eth.block_number}")
-
-    # Fill in your account here=
     try:
         balance = web3.eth.get_balance(address)
-        print(web3.from_wei(balance, "ether"))
+        return web3.from_wei(balance, "ether")
     except exceptions.InvalidAddress:
-        print("Invalid address!")
-    # try:
-    #     balance_wei = web3.eth.getBalance(address)
-    #     balance_eth = web3.fromWei(balance_wei, 'ether')
-    #     return f'Balance of {address}: {balance_eth} ETH'
-    # except Exception as e:
-    #     return f'Error: {e}'
-connect_status = web3.is_connected()
+        return None
 
-if connect_status:
-    get_balance("Wallet_Address")
-    print("Successfully connected!")
 
 if __name__ == '__main__':
     app.run(debug=True)
